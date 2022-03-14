3f7646d271aac33139f169c23a65702c08e34880
ConnorBStill <connor@lamarsoftware.io>
2022-03-14 05:41:37 -0700

    Start main screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as COLORS from './src/colors';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.BLACK,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/colors.ts
@@ -0,0 +1,6 @@
+export const BLACK = '#121213';
+export const DARK_YELLOW = '#b59f3b';
+export const GREEN = '#538d4e';
+export const WHITE = '#ffffff';
+export const DARK_GRAY = '#3a3a3c';
+export const GRAY = '#818384';
